e5f484d23c8ff1ecdd039574409c71a1deb8c3b7
Eli James <ejames@pandamian.com>
2013-09-01 23:42:17 +0800

    Removed initializers from application.rb

--- application.rb
+++ application.rb
@@ -6,11 +6,6 @@ require File.join(File.dirname(__FILE__), 'environment')
 
 configure do
   set :views, "#{File.dirname(__FILE__)}/views"
-
-  settings_yaml = YAML.load_file(settings.root + "/config.yml")[settings.environment.to_s]
-  settings_yaml.each_pair do |k, v|
-    set(k.to_sym, v)
-  end
 end
 
 error do
@@ -18,9 +13,6 @@ error do
   Kernel.puts e.backtrace.join("\n")
   'Application error'
 end
-
-# Data Mapper setup
-DataMapper.setup(:default, "mysql://#{settings.mysql_user}:#{settings.mysql_password}@localhost/orgtop_dev")
 
 helpers do
   # add your helpers here
