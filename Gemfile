c7b0d704d227dee82decd0a2dd724e71dab1739a
Eli James <ejames@pandamian.com>
2013-09-08 22:23:56 +0800

    Add parsing, handling and logging for received mails

--- Gemfile
+++ Gemfile
@@ -10,12 +10,13 @@ gem 'dm-aggregates'
 gem 'dm-migrations'
 gem 'dm-mysql-adapter'
 gem 'rerun'
-gem "chronic", "~> 0.10.1"
 
 gem "pony", "~> 1.4.1"
 gem "mailman", "~> 0.6.0"
 gem "clockwork", "~> 0.5.3"
 gem "stalker", "~> 0.9.0"
+gem "chronic", "~> 0.10.1"
+gem "mail_extract", "~> 0.1.4"
 
 group :test do
   gem 'rspec', :require => 'spec'
--- mailman_app.rb
+++ mailman_app.rb
@@ -1,9 +1,24 @@
 require 'mailman'
 require 'chronic'
+require 'mail_extract'
+require File.join(File.dirname(__FILE__), 'environment')
 
 settings_yaml = YAML.load_file("config.yml")["production"]
 
+def process_message(mail)
+  if mail.multipart?
+    part = mail.parts.select { |p| p.content_type =~ /text\/html/ }.first rescue nil
+    unless part.nil?
+      message = part.body.decoded
+    end
+  else
+    message = mail.body.decoded
+  end
+  MailExtract.new(message).body
+end
+
 Mailman.config.rails_root = nil
+#Mailman.config.logger = Logger.new("mailman.log")
 Mailman.config.pop3 = { username: settings_yaml["gmail_email"],
   password: settings_yaml["gmail_password"],
   server: 'pop.gmail.com',
@@ -11,18 +26,28 @@ Mailman.config.pop3 = { username: settings_yaml["gmail_email"],
   ssl: true
 }
 Mailman.config.graceful_death = true
-Mailman.config.poll_interval = 20
+Mailman.config.poll_interval = 10 # change to something logical for production
 
 Mailman::Application.run do
-  to 'orgtop@(linuxnus.org|nushackers.org)'.from('%user%@(linuxnus.org|nushackers.org)') do |username|
-    prompt_time = Chronic.parse("this week monday 7:30")
-    digest_time = Chronic.parse("this week monday 21:00")
+  from(/(.*)@(linuxnus.org|nushackers.org)/) do |username|
+    prompt_time = Chronic.parse(settings_yaml["prompt_time"])
+    digest_time = Chronic.parse(settings_yaml["digest_time"])
     current_time = Time.now
+
+    #Mailman.logger.info "Successfully entered method body with username #{username}"
+    #Mailman.logger.info "Prompt time: #{prompt_time}, Digest time: #{digest_time}, Current time: #{current_time} Boolean: #{current_time > prompt_time && current_time < digest_time}"
 
     # if current time is between nearest prompt time and digest time
     if current_time > prompt_time && current_time < digest_time
       user = User.first(username: username)
-      user.responses.create(content: message)
+      if user
+        msg_body = process_message(message)
+        user.responses.create(content: msg_body)
+      end
     end
+  end
+
+  default do
+    Mailman.logger.info "Hit default method instead."
   end
 end
