bd6eb5f7e5175942c5295218b516c62dd71ee6dc
Eli James <ejames@pandamian.com>
2013-09-09 00:08:24 +0800

    Switch back to plaintext processing.
    Also added digest formatting and copy.

--- mailman_app.rb
+++ mailman_app.rb
@@ -8,7 +8,7 @@ settings_yaml = YAML.load_file("config.yml")["production"]
 # Strips out mail headers and returns only the mail body
 def process_message(mail)
   if mail.multipart?
-    part = mail.parts.select { |p| p.content_type =~ /text\/html/ }.first rescue nil
+    part = mail.parts.select { |p| p.content_type =~ /text\/plain/ }.first rescue nil
     unless part.nil?
       message = part.body.decoded
     end
@@ -48,9 +48,6 @@ Mailman::Application.run do
     digest_time = Chronic.parse(settings_yaml["digest_time"])
     current_time = Time.now
 
-    #Mailman.logger.info "Successfully entered method body with username #{username}"
-    #Mailman.logger.info "Prompt time: #{prompt_time}, Digest time: #{digest_time}, Current time: #{current_time} Boolean: #{current_time > prompt_time && current_time < digest_time}"
-
     # if current time is between nearest prompt time and digest time
     if current_time > prompt_time && current_time < digest_time
       user = User.first(username: username)
@@ -62,6 +59,6 @@ Mailman::Application.run do
   end
 
   default do
-    Mailman.logger.info "Hit default method instead."
+    Mailman.logger.info "Email miss. Hit default method instead."
   end
 end
